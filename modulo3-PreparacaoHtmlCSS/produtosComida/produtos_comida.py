0f31d96c7d5e4bd406a45b4ad215622a7d25a839
Joabe Nascimento <joabe-nascimento>
2023-09-20 00:54:50 -0300

    Atividades atualizadas

---
+++ modulo3-PreparacaoHtmlCSS/produtosComida/produtos_comida.py
@@ -0,0 +1,9 @@
+# Array de produtos de uma loja de comida
+produtos_comida = ["Arroz", "Feijão", "Macarrão", "Carne", "Legumes"]
+
+# Array de anos de nascimento de familiares e amigos
+anos_nascimento = [1990, 1985, 1992, 1988, 2000]
+
+# Imprimir os arrays no terminal
+print("Produtos de uma loja de comida:", produtos_comida)
+print("Anos de nascimento de familiares e amigos:", anos_nascimento)
