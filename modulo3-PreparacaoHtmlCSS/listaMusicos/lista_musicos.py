0ce961a6cc15245008b9a870a6f47e4db360f550
Joabe Nascimento <joabe-nascimento>
2023-09-20 01:25:54 -0300

    Atividades atualizadas

---
+++ modulo3-PreparacaoHtmlCSS/listaMusicos/lista_musicos.py
@@ -0,0 +1,15 @@
+lista_musicos = ['Djavan', 'Roberto Carlos', 'Elis Regina', 'Tom Jobim', 'Milton Nascimento', 'Chico Buarque', 'Nara Leão', 'Pitty', 'Simonal', 'Moacir Santos', 'Caetano Veloso', 'Elza Soares', 'Paulinho da Viola', 'Yamandú Costa', 'Gal Costa']
+
+# Quantidade de elementos na lista
+quantidade_elementos = len(lista_musicos)
+
+# Dados nos índices especificados
+indice_2 = lista_musicos[2]
+indice_9 = lista_musicos[9]
+indice_14 = lista_musicos[14]
+
+# Impressão dos resultados
+print("Quantidade de elementos na lista:", quantidade_elementos)
+print("Dado no índice 2:", indice_2)
+print("Dado no índice 9:", indice_9)
+print("Dado no índice 14:", indice_14)
