6d36822ad883f9f3cc505805b65b7b97f520cb90
Joabe Nascimento <joabe-nascimento>
2023-09-21 22:00:13 -0300

    ativida atribuida de lista de produtos

---
+++ modulo3-PreparacaoHtmlCSS/listaProdutos/lista_produtos.py
@@ -0,0 +1,4 @@
+lista_produtos = ['máscaras faciais', 'batons', 'esmaltes', 'perfumes', 'loções', 'xampus', 'sabonetes', 'delineadores']
+
+for produto in lista_produtos:
+    print(f'Temos {produto} à venda!')
